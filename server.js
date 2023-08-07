f3f5742bc08414743a182b554684db071c046bd6
Shahar Saadon <shahars@moveo.co.il>
2023-08-07 12:19:35 +0300

    server/implement logger

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const http = require("http").createServer(app);
 app.use(express.json());
 const port = process.env.PORT || 3030;
 
+// const authRoutes = require("./api/auth/auth.routes");
+
+const logger = require("./services/logger.service");
+logger.info("Hi", 90, "Bobo");
+
 http.listen(port, () => {
     console.log("Server is running on port: " + port);
 });
